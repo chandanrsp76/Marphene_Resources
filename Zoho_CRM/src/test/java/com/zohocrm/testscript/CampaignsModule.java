bfec0c0f82f17337f9dcad3602d402c784f66cdf
chandan <chandan@DESKTOP-O44MIID>
2023-04-14 16:42:39 +0530

    first commit

---
+++ Zoho_CRM/src/test/java/com/zohocrm/testscript/CampaignsModule.java
@@ -0,0 +1,11 @@
+package com.zohocrm.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CampaignsModule {
+@Test
+public void createCampaign() {
+	Reporter.log("CreateCampaign",true);
+}
+}
