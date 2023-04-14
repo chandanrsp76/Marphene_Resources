a438d1257e4e933f02a9ba287fd03cd0f9072ed0
chandan <chandan@DESKTOP-O44MIID>
2023-04-14 23:28:04 +0530

    LeadModule

---
+++ Zoho_CRM/src/test/java/com/zohocrm/testscript/LeadModule.java
@@ -0,0 +1,11 @@
+package com.zohocrm.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LeadModule {
+@Test
+public void createNewLead() {
+	Reporter.log("Create New Lead",true);
+}
+}
